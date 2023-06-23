08d5e92d3776f4472cd7ede496da0233e35022d2
Nithe14 <imnithe@gmail.com>
2023-06-24 00:08:17 +0200

    fix undo dir

--- lua/variables.lua
+++ lua/variables.lua
@@ -1,2 +1,7 @@
-vim.o.undodir = '/home/nithe/.config/nvim/undo'
+local sysname = vim.loop.os_uname().sysname
+if sysname == 'Darwin' then
+   vim.o.undodir = '/Users/nithe/.config/nvim/undo'
+else
+   vim.o.undodir = '/home/nithe/.config/nvim/undo'
+end
 vim.o.undofile = true
