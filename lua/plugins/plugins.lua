b3c36abfc1d9d377298289f8be14747bc8c5eb93
nithe14 <imnithe@gmail.com>
2023-07-04 09:10:53 +0200

    using telescope 0.1.2 now

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -18,7 +18,7 @@ return require('packer').startup(function(use)
 	requires = { 'nvim-tree/nvim-web-devicons', opt = true }
 }
 use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+  'nvim-telescope/telescope.nvim', tag = '0.1.2',
    requires = { 'nvim-lua/plenary.nvim' }
 }
 use {'neoclide/coc.nvim', branch = 'release'}
