50be19f2885f824455f32089ae2ab8c264a80d13
nithe14 <imnithe@gmail.com>
2023-10-11 09:00:58 +0200

    edit toggler binds

--- lua/plugins/plugins_config/toggler.lua
+++ lua/plugins/plugins_config/toggler.lua
@@ -2,7 +2,9 @@
 require('nvim-toggler').setup({
   -- your own inverses
   inverses = {
-    ['let'] = 'let mut'
+    ['let'] = 'let mut',
+    ['white'] = 'black',
+    ['0'] = '1'
   },
   -- removes the default <leader>i keymap
   remove_default_keybinds = false,
